ee78bedf3b767d599374552d1955c9bf0f0e374d
Ade <aakano684>
2020-07-19 02:20:56 +0100

    yaml update

--- dequeue-question/index.js
+++ dequeue-question/index.js
@@ -1,42 +1,41 @@
 const { QueueServiceClient, StorageSharedKeyCredential } = require("@azure/storage-queue");
 require("dotenv").config();
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            status: 400,
-            body: "no request parameters expected"
-        };
-
-
-    }
-    else {
-      return {
-        httpResponse: {
-            body: dequeue()
-        },
-        queueOutput: retMsg
+  if (req.query.name || (req.body && req.body.name)) {
+    context.res = {
+      status: 400,
+      body: "no request parameters expected"
     };
-    }
+
+
+  }
+  else {
+    return {
+      httpResponse: {
+        body: dequeue()
+      }
+    };
+  }
 };
 
 
 
- async function dequeue() {
+async function dequeue() {
 
   const account = process.env.ACCOUNT_NAME || "";
   const accountKey = process.env.ACCOUNT_KEY || "";
 
   const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
 
- 
+
   const queueServiceClient = new QueueServiceClient(
     `https://${account}.queue.core.windows.net`,
     sharedKeyCredential
   );
 
-  
+
   const queueName = process.env.QUEUE_NAME;
   const queueClient = queueServiceClient.getQueueClient(queueName);
 
